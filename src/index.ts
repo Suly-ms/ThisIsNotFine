39b0b86378f1731da6cb29c5a84436ac5c551a42
mschnider <martin.schnidersylvestre@gmail.com>
2026-02-06 16:53:19 +0100

    Modif mail accepté pour accepter tout type de mail étudiant, ajour page de mails acceptés

--- src/index.ts
+++ src/index.ts
@@ -57,9 +57,10 @@ app.use((req, res, next) => {
         '/style.css',
         '/api/schools',
         '/about.html',
-        '/search.html', // Autorise la page de recherche
+        '/search.html',
         '/login.html',
-        '/school.html'  // Autorise la page qui affiche les étudiants
+        '/school.html',
+        '/email-correct.html'  
     ];
 
     if (publicRoutes.includes(req.path) || req.path.startsWith('/api/schools/')) {
@@ -178,9 +179,118 @@ transporter.verify((error, success) => {
 app.post('/api/signup', async (req, res) => {
     const { firstname, lastname, schoolId, searchType, searchStatus, password, email } = req.body;
 
-    // Validation email unistra
-    if (!email || !email.endsWith('@etu.unistra.fr')) {
-        return res.status(400).send('Seules les adresses @etu.unistra.fr sont autorisées.');
+    // Liste des principaux domaines académiques français
+    // À mettre à jour si des étudiants vous signalent un manquant.
+    const allowedDomains = [
+        // --- Universités & Académies ---
+        'etu.unistra.fr',        // Strasbourg
+        'unistra.fr',
+        'etu.sorbonne-universite.fr', // Sorbonne
+        'sorbonne-universite.fr',
+        'etu.u-paris.fr',        // Université de Paris
+        'u-paris.fr',
+        'etu.univ-lyon1.fr',     // Lyon 1
+        'univ-lyon1.fr',
+        'etu.univ-lyon2.fr',     // Lyon 2
+        'univ-lyon2.fr',
+        'etu.univ-lyon3.fr',     // Lyon 3
+        'univ-lyon3.fr',
+        'etu.univ-amu.fr',       // Aix-Marseille
+        'univ-amu.fr',
+        'etu.u-bordeaux.fr',     // Bordeaux
+        'u-bordeaux.fr',
+        'etu.univ-lille.fr',     // Lille
+        'univ-lille.fr',
+        'etu.univ-nantes.fr',    // Nantes
+        'univ-nantes.fr',
+        'etu.univ-rennes1.fr',   // Rennes 1
+        'univ-rennes1.fr',
+        'etu.univ-rennes2.fr',   // Rennes 2
+        'univ-rennes2.fr',
+        'etu.univ-toulouse.fr',  // Toulouse
+        'univ-toulouse.fr',
+        'etu.univ-grenoble-alpes.fr', // Grenoble
+        'univ-grenoble-alpes.fr',
+        'etu.univ-cotedazur.fr', // Nice / Côte d'Azur
+        'univ-cotedazur.fr',
+        'etu.univ-lorraine.fr',  // Lorraine (Nancy/Metz)
+        'univ-lorraine.fr',
+        'etu.univ-montpellier.fr', // Montpellier
+        'univ-montpellier.fr',
+        'etu.unicaen.fr',        // Caen
+        'unicaen.fr',
+        'etu.u-picardie.fr',     // Amiens (Picardie)
+        'u-picardie.fr',
+        'etu.univ-poitiers.fr',  // Poitiers
+        'univ-poitiers.fr',
+        'etu.univ-tours.fr',     // Tours
+        'univ-tours.fr',
+        'etu.univ-orleans.fr',   // Orléans
+        'univ-orleans.fr',
+
+        // --- Écoles Tech / Informatique ---
+        'epitech.eu',            // Epitech (National)
+        'student.42.fr',         // École 42 (Paris)
+        '42.fr',
+        'student.42lyon.fr',     // 42 Lyon
+        'student.42nice.fr',     // 42 Nice
+        'supinfo.com',           // Supinfo
+        'epita.fr',              // EPITA
+        'esiea.fr',              // ESIEA
+        'efrei.fr',              // EFREI
+        'ece.fr',                // ECE
+        'etna.io',               // ETNA
+
+        // --- Grandes Écoles d'Ingénieurs ---
+        'polytechnique.edu',     // Polytechnique (X)
+        'centralesupelec.fr',    // CentraleSupélec
+        'mines-paristech.fr',    // Mines Paris
+        'telecom-paris.fr',      // Télécom Paris
+        'enpc.fr',               // Ponts ParisTech
+        'insa-lyon.fr',          // INSA Lyon
+        'insa-toulouse.fr',      // INSA Toulouse
+        'insa-rennes.fr',        // INSA Rennes
+        'insa-rouen.fr',         // INSA Rouen
+        'insa-strasbourg.fr',    // INSA Strasbourg
+        'utbm.fr',               // UTBM
+        'utc.fr',                // UTC
+        'utt.fr',                // UTT
+        'ens.fr',                // ENS Ulm
+        'ens-lyon.fr',           // ENS Lyon
+        'ens-paris-saclay.fr',   // ENS Paris-Saclay
+        'arts-et-metiers.fr',    // Arts et Métiers (ENSAM)
+
+        // --- Grandes Écoles de Commerce ---
+        'hec.edu',               // HEC Paris
+        'essec.edu',             // ESSEC
+        'edhec.com',             // EDHEC
+        'em-lyon.com',           // EM Lyon
+        'escpeurope.eu',         // ESCP
+        'audencia.com',          // Audencia
+        'grenoble-em.com',       // GEM
+        'neoma-bs.fr',           // NEOMA
+        'kedgebs.com',           // KEDGE
+        'skema.edu',             // SKEMA
+        'tbs-education.fr',      // Toulouse BS
+        
+        // --- Sciences Po ---
+        'sciencespo.fr',         // Sciences Po Paris (+ campus régionaux)
+        'sciencespo-lille.eu',   // Sciences Po Lille
+        'sciencespo-lyon.fr',    // Sciences Po Lyon
+        'sciencespo-bordeaux.fr' // Sciences Po Bordeaux
+    ];
+
+    // 1. Récupérer le domaine de l'email
+    const emailDomain = email.split('@')[1];
+
+    // 2. Vérifier si le domaine est dans notre liste
+    if (!email || !allowedDomains.includes(emailDomain)) {
+        console.log(`Inscription refusée pour le domaine : ${emailDomain}`);
+        
+        return res.status(400).send(
+            `Désolé, l'école liée à "@${emailDomain}" n'est pas encore reconnue.\n` +
+            `Envoyez un mail à thisisnotfine.noreply@gmail.com pour l'ajouter !`
+        );
     }
 
     if (!password || password.length < 3) {
